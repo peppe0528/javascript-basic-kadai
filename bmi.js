2baf75763416f27255765e3efca0590d72b3ee87
peppe0528 <phara240@gmail.com>
2024-09-02 14:17:39 +0900

    課題の提出

--- bmi.js
+++ bmi.js
@@ -2,4 +2,6 @@ let userWeight = 68;
 
 let userHeight = 1.7;
 
-console.log([userWeight] / [userHeight * userHeight]);
+let userBMI = userWeight / (userHeight * userHeight);
+
+console.log(userBMI);
