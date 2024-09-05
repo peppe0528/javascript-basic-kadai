a178a9cdc81633255cfbd93bfb6f0bcf6d5e0cb7
peppe0528 <phara240@gmail.com>
2024-09-05 19:52:46 +0900

    Delete event.js

--- event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-
-const text = document.getElementById('text')
-
-btn.addEventListener('click', () => {
-    const childList  = document.createElement('li');
-    text.textContent = 'ボタンをクリックしました';
-});
